141fb1431b38ca75c38b07e743affe491ed3b64b
Jakub Westfalewski <j.westfalewski@gmail.com>
2023-01-26 22:22:30 +0100

    add env

--- run.ts
+++ run.ts
@@ -1,7 +1,9 @@
 import omnivoreClient from "./omnivore-client.ts";
 
 async function readArticles() {
-  const articles = await omnivoreClient(Deno.args[0]);
+  const articles = await omnivoreClient(
+    Deno.env.get("OMNIVORE_API_TOKEN") || Deno.args[0],
+  );
   return articles;
 }
 
